3b696392c836b7728723fb6a38197cb28ca665a2
Qun Ying <quying@microsoft.com>
2017-08-01 17:01:49 +0800

    script

--- CRA/setupWeb.sh
+++ CRA/setupWeb.sh
@@ -6,7 +6,15 @@ sudo apt-get -y install nodejs
 sudo apt-get -y install npm
 sudo apt-get -y install git
 
+cd ~
+git clone https://github.com/yingqunpku/azure-stack-anomaly-detection.git
+cd azure-stack-anomaly-detection/CRA/WebApp/routes/
 
-
+#TODO: parameterize the following two params
+sudo sed -i "s/ADSHOST/192.168.102.16/g" score.js
+sudo sed -i "s/ADSPORT/8888/g" score.js
+cd ~/azure-stack-anomaly-detection/CRA/WebApp
+./run.sh
 
 sudo iptables -I INPUT -p tcp --dport 8080 -j ACCEPT #Portal Web
+#TODO: open port 8080 on the nsg for inbound
